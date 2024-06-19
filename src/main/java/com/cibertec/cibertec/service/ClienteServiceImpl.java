59881ebe7752058fb6bb8e2aee4d665e305ebc63
renato <jorgejacinto9701@gmail.com>
2024-06-18 20:33:18 -0500

    mensajes

--- src/main/java/com/cibertec/cibertec/service/ClienteServiceImpl.java
+++ src/main/java/com/cibertec/cibertec/service/ClienteServiceImpl.java
@@ -1,12 +1,14 @@
 package com.cibertec.cibertec.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cibertec.cibertec.entity.Cliente;
 import com.cibertec.cibertec.repository.ClienteRepository;
+
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
@@ -18,5 +20,37 @@ public class ClienteServiceImpl implements ClienteService {
     public List<Cliente> listaCliente() {
             return repository.findAll();
     }
+
+    @Override
+    public Cliente insertaActualizaCliente(Cliente obj) {
+       return repository.save(obj);
+    }
+
+    @Override
+    public void eliminaCliente(int id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Cliente> obtenerId(int id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Cliente> listaClientePorNombre(String nombres) {
+    return repository.findByNombresLike(nombres);
+    }
+
+    @Override
+    public List<Cliente> listaClientePorDni(String dni) {
+       return repository.findByDni(dni);
+    }
+
+    @Override
+    public List<Cliente> listaClientePorDniNombre(String dni, String nombres) {
+       return repository.findByDniAndNombres(dni, nombres);
+    }
+
     
+
 }
